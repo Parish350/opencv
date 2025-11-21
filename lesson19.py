e4b3456a3b2ca94821553d78642e72e06c6c560e
Parish350 <parishpugazh@gmail.com>
2025-11-21 18:02:12 +0000

    Create lesson19.py

---
+++ lesson19.py
@@ -0,0 +1,23 @@
+import cv2,os
+haar_file = "facedetector.xml"
+datasets = "C:/Users/ppuga\Documents/jetlearn/opencv/Datasets/Hashmi"
+# os.chdir (datasets)
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam = cv2.VideoCapture(0)
+Count = 0 
+while Count <= 30: 
+ ret,img = webcam.read()
+# Convert an image from RGB to HSV
+ grayImage = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ faces = face_cascade.detectMultiScale(grayImage,1.3,4)
+ for (x,y,w,h) in faces: 
+    cv2.rectangle(img,(x,y),(x + w, y + h),(255,0,0),2)
+    face = grayImage[y:y +h, x:x + w]
+    face_resize = cv2.resize(face, (100,130))
+    os.chdir(datasets)
+    cv2.imwrite(f'{Count}.png',face_resize)
+ Count += 1
+ cv2.imshow('screen',img)
+ K = cv2.waitKey(10)
+ if K == 27:
+        break
