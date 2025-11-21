a09299014049f23cfcad9483038aa75816cf1f89
Parish350 <parishpugazh@gmail.com>
2025-11-21 17:39:18 +0000

    Create lesson18.py

---
+++ lesson18.py
@@ -0,0 +1,20 @@
+import cv2
+import cv2,numpy,os
+haar_file = "facedetector.xml"
+datasets = 'Datasets'
+(images,labels,names,id) = ([],[],{},0)
+for (subdirs,dirs,files)in os.walk(datasets):
+ for subdir in dirs:
+  names[id] = subdir
+  subjectpath = os.path.join(datasets,subdir)
+  for filename in os.listdir(subjectpath):
+   path = subjectpath +'/'+ filename
+   images.append(cv2.imread(path,0))
+   label = id
+   labels.append(int(label))
+   id += 1
+#(images,labels) = [numpy.array(list)for list in[images,labels]]
+images = numpy.array(images)
+labels = numpy.array(labels)
+recoginser = cv2.face.LBPHFaceRecognizer_create()
+recoginser.train(images,labels)
